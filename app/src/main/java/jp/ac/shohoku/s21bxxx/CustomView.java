7f7ed9f3477b499a962ad46db23c8c8f4890dbf6
unknown <utsumi@shohoku.ac.jp>
2021-10-29 11:35:56 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/shohoku/s21bxxx/CustomView.java
@@ -0,0 +1,21 @@
+package jp.ac.shohoku.s21bxxx;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CustomView extends View {
+    public CustomView(Context context) {
+        super(context);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
